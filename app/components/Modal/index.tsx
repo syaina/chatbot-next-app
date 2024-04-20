dfa8551e17ffd49ac478128a8b07ce023eca63a3
Syaina Nur <syaina@femaledaily.com>
2024-04-21 03:02:38 +0700

    feat: add close button on modal

--- app/components/Modal/index.tsx
+++ app/components/Modal/index.tsx
@@ -2,11 +2,19 @@ interface Modal {
   id: string;
   title: string;
   children: JSX.Element;
+  isShowCloseButton?: boolean;
 }
 
 export const Modal = (props: Modal) => (
-  <dialog id="modal-delete" className="modal">
+  <dialog id={props.id} className="modal">
     <div className="modal-box">
+      {props.isShowCloseButton && (
+        <form method="dialog">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+      )}
       <h3 className="font-lg-subtitle-1-bold pb-2">{props.title}</h3>
       {props.children}
     </div>
